0f37885adc4417ae1317278cd3870ee071e0fe57
cho <chokyungyeul@gmail.com>
2022-07-12 11:23:25 +0900

    commit 2022-07-12.md

---
+++ gitpush.sh
@@ -0,0 +1,23 @@
+
+#git config --global user.email "chokyungyeul@gmail.com"
+#git config --global user.name "cho"
+#token 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
+
+#gitpush.sh
+Y=$(date +%Y)
+M=$(date +%m)
+D=$(date +%d)
+ 
+Ym=$Y-$M
+Ymd=$Y-$M-$D
+GitRep="Azure_infra"
+ 
+HomeDir="/mnt/c/terraform/terraformTogithub"
+GitDir="$HomeDir/$GitRep"
+FileDir="$HomeDir/$GitRep/$Ym"
+FileNm="$Ymd".md
+ 
+cd $GitDir
+git add .
+git commit -m "commit $FileNm"
+git push terraform master
