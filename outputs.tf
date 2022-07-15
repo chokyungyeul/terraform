f30fe4d34212a7d41dc1d88cd02e3c6f8d0d8579
cho <chokyungyeul@gamil.com>
2022-07-15 14:58:18 +0900

    commit prd_v53

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@
 # #   value = module.StorageAccount.stg_act_name_out
 # # }
 
-output "RgName" {
-  value = module.ResourceGroup[*].rg_name_out
-}
+# output "RgName" {
+#   value = module.ResourceGroup[*].rg_name_out
+# }
 
 #output "vm_id" {
 #  value       = module.vm01[*].vm_id_out
